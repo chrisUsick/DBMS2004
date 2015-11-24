cd13235c8c241d10c9b26f44e68416c91da06b76
Christopher Usick <christopher.usick@gmail.com>
2015-11-24 16:19:08 -0600

    started 6C

--- asgn6/C/Usick_6C.sql
+++ asgn6/C/Usick_6C.sql
@@ -11,9 +11,14 @@ AS
   CURSOR tables IS 
   SELECT table_name 
   FROM User_Tables
+	WHERE table_name NOT LIKE 'BIN%'
   ORDER BY table_name;
   
   wTable tables%ROWTYPE;
+	
+	-- exception type 
+	INVALID_COLUMN_TYPE EXCEPTION;
+	PRAGMA EXCEPTION_INIT(INVALID_COLUMN_TYPE, -20100);
 BEGIN
   -- initial output
   DBMS_OUTPUT.PUT_LINE(cTITLE);
@@ -41,7 +46,19 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(wCreateStatement);
     
     -- extract columns
-    Extract_Columns(wTable.table_name);
+		BEGIN -- catch exceptions thrown by extract columns
+			Extract_Columns(wTable.table_name);
+			
+			-- extract constraints
+			Extract_PK_Constraint(wTable.table_name);
+			Extract_Unique_Constraint(wTable.table_name);
+			EXCEPTION
+				WHEN INVALID_COLUMN_TYPE THEN
+					DBMS_OUTPUT.PUT_LINE('==========================================================================');
+					DBMS_OUTPUT.PUT_LINE('=== EXCEPTION ' || SQLCODE || ' Raised - ORA-20100: *** ' || SQLERRM || ' ***');
+					DBMS_OUTPUT.PUT_LINE('=== Unable to complete table generation for ' || wTable.table_name);
+					DBMS_OUTPUT.PUT_LINE('==========================================================================');
+		END;
     
     -- add the closing parenthesis
     DBMS_OUTPUT.PUT_LINE(
@@ -58,6 +75,103 @@ BEGIN
   
 END;
 /
+exec extract_tables;
+exec extract_pk_constraint('PRODUCTS');
+-- Extract primary key constraint for a given table
+CREATE OR REPLACE PROCEDURE Extract_PK_Constraint (
+  iTableName IN VARCHAR2)
+AS
+  wOutput VARCHAR2(200) := '';
+BEGIN
+	wOutput := Extract_Constraint(iTableName, 'P', 'PRIMARY KEY');
+	DBMS_OUTPUT.PUT_LINE(wOutput);
+	
+END;
+/
+
+exec Extract_Unique_Constraint('PRODUCTS');
+-- Extract unique constraint(s) for a given table
+CREATE OR REPLACE PROCEDURE Extract_Unique_Constraint (
+  iTableName IN VARCHAR2)
+AS
+  wOutput VARCHAR2(200) := '';
+BEGIN
+	wOutput := Extract_Constraint(iTableName, 'U', 'UNIQUE');
+	DBMS_OUTPUT.PUT_LINE(wOutput);
+	
+END;
+/
+
+-- Extract unique constraint(s) for a given table
+CREATE OR REPLACE PROCEDURE Extract_Check_Constraint (
+  iTableName IN VARCHAR2)
+AS
+	wConstraintName VARCHAR2(50);
+	wSearchCondition VARCHAR2(10000);
+	wOutput VARCHAR2(10000) := '';
+	wSearchCast VARCHAR2(10000);
+BEGIN
+	OPEN Consts;
+	LOOP 
+		FETCH Consts INTO wConstraintName, wSearchCondition;
+		EXIT WHEN Consts%NOTFOUND;
+		wSearchCondition := wSearchCondition;
+		wSearchCast := wSearchCondition || ' ';
+		
+		wOutput := wOutput || '  , CONSTRAINT ' || 
+							wConstraintName || CHR(10) || '       ' || 
+							iTypeText || ' (';
+		DBMS_OUTPUT.PUT_LINE(wSearchCast);
+	END LOOP;
+	
+END;
+/
+
+exec Extract_Check_Constraint('PRODUCTS');
+CREATE OR REPLACE FUNCTION Extract_Constraint (
+	iTableName IN VARCHAR2, 
+	iConstraintType IN VARCHAR2, 
+	iTypeText IN VARCHAR2) RETURN VARCHAR2
+AS
+	wFirstColumn NUMBER(1) := 0;
+	wOutput VARCHAR2(10000) := '';
+BEGIN
+	FOR wConst IN (
+		SELECT Constraint_Name
+		FROM User_Constraints
+		WHERE Table_Name = iTableName 
+		AND Constraint_Type = iConstraintType
+	) LOOP
+		
+		wOutput := wOutput || '  , CONSTRAINT ' || 
+							wConst.Constraint_Name || CHR(10) || '       ' || 
+							iTypeText || ' (';
+							
+		wFirstColumn := 0;
+		IF (iConstraintType = 'C') THEN 
+			wOutput := wOutput || ' '; --wSearchCondition;
+		ELSE
+			FOR wColumn IN (
+				SELECT Column_Name
+				FROM User_Cons_Columns
+				WHERE Table_Name = iTableName
+				AND Constraint_Name = wConst.Constraint_Name
+			) LOOP 
+				IF wFirstColumn = 0 THEN
+					wFirstColumn := 1;
+					wOutput := wOutput || wColumn.Column_Name;
+				ELSE 
+					wOutput := wOutput || ', ' || wColumn.Column_Name;
+				END IF;
+			END LOOP;
+			
+		END IF;
+		wOutput := wOutput || ')' || CHR(10);
+	END LOOP;
+	
+	RETURN SUBSTR(wOutput, 0, LENGTH(wOutput) - 1);
+END;
+/
 
 -- Create and output all the drop tables statements for all tables in schema
 CREATE OR REPLACE PROCEDURE Drop_Tables 
@@ -65,6 +179,7 @@ AS
   CURSOR tables IS 
   SELECT table_name 
   FROM User_Tables
+	WHERE table_name NOT LIKE 'BIN%'
   ORDER BY table_name;
   
   wTable tables%ROWTYPE;
@@ -142,7 +257,8 @@ BEGIN
             wDefault
             ;
     ELSE
-      wOutput := wOutput || ' *** Unknown data type *** ';
+			-- throw error
+			RAISE_APPLICATION_ERROR(-20100, 'Unknown data type: ' || wColumn.Data_Type);
     END IF;
     -- ouput the output string
     DBMS_OUTPUT.PUT_LINE('  ' || wOutput);
